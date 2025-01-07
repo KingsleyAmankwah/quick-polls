57e9294bfd988fe5efcc1fb474dac3a559121634
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-01-07 19:07:36 +0000

    chore: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { PollListComponent } from './features/poll-list/poll-list.component';
+import { CreatePollComponent } from './features/create-poll/create-poll.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PollListComponent,
+  },
+  {
+    path: 'create',
+    component: CreatePollComponent,
+  },
+  {
+    path: 'polls/:id',
+    loadComponent: () =>
+      import('./features/poll-details/poll-details.component').then(
+        (m) => m.PollDetailsComponent
+      ),
+  },
+];
